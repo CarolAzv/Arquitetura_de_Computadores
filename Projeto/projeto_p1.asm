658ac498fcac64c262ddb2ffcc22647f9363e2f1
Carol <caroldazevedo52@gmail.com>
2025-06-17 15:58:55 -0300

    Add files via upload

---
+++ Projeto/projeto_p1.asm
@@ -0,0 +1,21 @@
+.text
+main: lui $8, 0x1001
+
+      lui $5, 0x00ff
+      ori $5, $5, 0xffff
+      addi $2, $0, 42
+      
+      addi $10, $0, 512
+      sll $9, $9, 8
+      
+test: beq $10, $0, fim
+      addi $4, $0, 0
+      syscall
+      sw $4, 0($8)
+      
+      addi $8, $8, 4
+      addi $10, $10, -1
+      j test      
+      
+fim:  addi $2, $0, 10
+      syscall
